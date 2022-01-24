aac4ad3a18d7a2be9bf623d849622d0515adcac0
Suman  Chakraborty <tht.suman@gmail.com>
2022-01-24 22:25:59 +0530

    Updates

---
+++ precache-manifest.31b9bc39103a4ef996b39101cca05053.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b8229fd03a3d386e4f0607c66c39de5b",
+    "url": "/gsap-website-practice/index.html"
+  },
+  {
+    "revision": "f15a10fa474a3a3bdc36",
+    "url": "/gsap-website-practice/static/css/main.c6a82108.chunk.css"
+  },
+  {
+    "revision": "c422d8f83a6a256ea89a",
+    "url": "/gsap-website-practice/static/js/2.7aadb8e4.chunk.js"
+  },
+  {
+    "revision": "3cd4b6bc4f51e64816d8a2b69bba1790",
+    "url": "/gsap-website-practice/static/js/2.7aadb8e4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f15a10fa474a3a3bdc36",
+    "url": "/gsap-website-practice/static/js/main.43339f9b.chunk.js"
+  },
+  {
+    "revision": "b5ffcfc8284a221dff41",
+    "url": "/gsap-website-practice/static/js/runtime-main.c16f3ddd.js"
+  },
+  {
+    "revision": "f9a44e1ccb9f44cd4de1da8fe7f6f4d8",
+    "url": "/gsap-website-practice/static/media/arrow-left.f9a44e1c.svg"
+  },
+  {
+    "revision": "dc372a07777e87891aaac087cfbe60ad",
+    "url": "/gsap-website-practice/static/media/arrow-right.dc372a07.svg"
+  }
+]);
